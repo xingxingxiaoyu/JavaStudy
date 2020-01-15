2b2b7f330169bfdb4c7d1f3fa7e3c13344e61284
xujiafeng <xujiafeng_31415926@163.com>
2020-01-15 22:38:50 +0800

    今日一问相关代码

---
+++ src/daily_question/day3/IntegerNode.java
@@ -0,0 +1,14 @@
+package daily_question.day3;
+
+import common.BaseNode;
+import common.SingleNode;
+
+public class IntegerNode extends SingleNode<Integer> {
+    public IntegerNode(Integer value) {
+        super(value);
+    }
+
+    public IntegerNode(Integer value, SingleNode<Integer> next) {
+        super(value, next);
+    }
+}
