90a92a154d06971fe1199734139b5fbfe977a22b
徐郁 <xuyu@konka.com>
2020-06-02 19:31:39 +0800

    android34课学习

--- src/other/Util.java
+++ src/other/Util.java
@@ -7,18 +7,19 @@ import java.util.List;
 
 public class Util {
     public static void main(String[] args) {
-        File file = new File("D:\\old\\OTA压测_摄像头OTA升级成功率_20200513212442\\OTA压测_摄像头OTA升级成功率_20200513212442\\fail_logs");
-        File[] files = file.listFiles();
-        List<Integer> integers = new ArrayList<>();
-        for (int i = 0; i < files.length; i++) {
-            String name = files[i].getName();
-            String s = name.substring(name.indexOf("第") + 1, name.indexOf("次"));
-            integers.add(Integer.valueOf(s));
-        }
-        Object[] a = integers.toArray();
-        Arrays.sort(a);
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
-        }
+//        File file = new File("D:\\old\\OTA压测_摄像头OTA升级回滚压测_20200527181621\\OTA压测_摄像头OTA升级回滚压测_20200527181621\\fail_logs");
+//        File[] files = file.listFiles();
+//        List<Integer> integers = new ArrayList<>();
+//        for (int i = 0; i < files.length; i++) {
+//            String name = files[i].getName();
+//            String s = name.substring(name.indexOf("第") + 1, name.indexOf("次"));
+//            integers.add(Integer.valueOf(s));
+//        }
+//        Object[] a = integers.toArray();
+//        Arrays.sort(a);
+//        for (int i = 0; i < a.length; i++) {
+//            System.out.println(a[i]);
+//        }
+        System.out.println(197960614 % 1024 + 1166);
     }
 }
